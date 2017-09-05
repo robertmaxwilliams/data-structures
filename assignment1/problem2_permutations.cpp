ef141c95fd78632fc8c4801fb8d577703681c104
robertmaxwilliams <robertmaxwilliams@gmail.com>
2017-09-05 19:14:08 -0400

    added other problems

---
+++ assignment1/problem2_permutations.cpp
@@ -0,0 +1,28 @@
+/*
+ * Write the routines with the following declarations:
+ * void permute( const string & str );
+ * void permute( const string & str, int low, int high );
+ * The first routine is a driver that calls the second and prints all the permutations of the characters
+ * in string str. If str is "abc", then the strings that are output are abc, acb, bac, bca, cab, and
+ * cba. Use recursion for the second routine. 2
+ * How many permutations for a string of length l?
+ */
+
+
+#include <iostream>
+using namespace std;
+
+void permute(const string &str){
+    
+    
+
+}
+
+void permute(const string &str, int high, int low){
+
+
+}
+
+int main(){
+
+}
