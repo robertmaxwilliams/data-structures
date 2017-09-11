aa1d4734a922d3ed9c4bd81ec7c709a0e71ae1d7
robertmaxwilliams <robertmaxwilliams@gmail.com>
2017-09-11 14:30:38 -0400

    finished collection calss

--- assignment1/problem4_collection.cpp
+++ assignment1/problem4_collection.cpp
@@ -6,33 +6,139 @@
  * present in the collection.
  */
 
+#include <cstring>
+#include <iostream>
+#include <sstream>      // std::stringstream
+
+using namespace std;
+
 template <typename Object>
 class Collection
 {
     public:
 
-        InitCell(){
-            collection = {};
-            size = 0;
+        Collection(){
+            makeEmpty(); 
         }
 
+        Object at(int i){
+            if (i >= size){
+                throw invalid_argument("out of range");
+            }
+            else if (i < 0){
+                throw invalid_argument("negative index");
+            }
+            else
+                return collection[i];
+        }
             
 
         bool isEmpty(){
-            return this.size == 0;
+            return (size == 0);
         }
 
         void makeEmpty(){
-            this.size = 0;
-            this.collection = {};
+            size = 0;
+            collection = new Object[size];
         }
 
-        void insert(Object x){
-            Object * temp;
-            temp = new Object [++size];
-            memcpy(temp, 
+        string tostring(){
+            stringstream buf;
+            buf << "[";
 
+            for (int i = 0; i < size; i++){
+                buf << collection[i];
+
+                if (i != size-1)
+                    buf << ", ";
+            }
+            buf << "]";
+
+            return buf.str();
+        }
+
+        void append(Object x){
+           insert(x, size);
+        }
+
+        void insert(Object x, int i){
+            Object * temp;
+            temp = new Object[size+1];
+            memcpy(temp, collection, i*sizeof(Object));
+            memcpy(temp+i+1, collection+i, (size-i)*sizeof(Object));            
+            temp[i] = x;
+            delete [] collection;
+            collection = temp;
+            size += 1;
+        }
+
+        void remove(int i){
+            Object * temp;
+            temp = new Object[size-1];
+            memcpy(temp, collection, i*sizeof(Object));
+            memcpy(temp+i, collection+i+1, (size-i-1)*sizeof(Object));            
+            delete [] collection;
+            collection = temp;
+            size -= 1;
+        }
+
+        bool contains(Object x){
+            for (int i = 0; i < size; i++){
+                if (collection[i] == x)
+                    return true;
+            }
+            return false;
+        }
 
     private:
-        Object collection[];
-        int size = 0;
+        Object* collection;
+        int size;
+
+};
+
+int main()
+{
+    Collection<char> foo;
+    foo.append('a');
+    cout << foo.tostring() << endl;
+
+    foo.append('b');
+    cout << foo.tostring() << endl;
+
+    foo.append('c');
+    cout << foo.tostring() << endl;
+    
+    foo.insert('x', 1);
+    cout << foo.tostring() << endl;
+
+    foo.remove(1);
+    cout << foo.tostring() << endl;
+
+    cout << "foo.contains('b') = " << foo.contains('b') << endl;
+    cout << "foo.contains('x') = " << foo.contains('x') << endl;
+
+    cout << endl;
+
+    Collection<int> bar;
+    bar.append(10);
+    cout << bar.tostring() << endl;
+
+    bar.append(20);
+    cout << bar.tostring() << endl;
+
+    bar.append(30);
+    cout << bar.tostring() << endl;
+
+    bar.insert(99, 1);
+    cout << bar.tostring() << endl;
+
+    bar.remove(1);
+    cout << bar.tostring() << endl;
+
+    cout << "bar.contains(20) = " << bar.contains(20) << endl;
+    cout << "bar.contains(99) = " << bar.contains(99) << endl;
+    cout << "bar.isEmpty() = " << bar.isEmpty() << endl;
+    
+
+   
+}
